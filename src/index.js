81c8023d41a8e483b86d4be5ac6a7b3465bb3b7a
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-03-28 06:00:30 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+  
   const createTaskForm = document.getElementById('create-task-form');
   const newTaskDescription = document.getElementById('new-task-description');
+  const newTaskPriority = document.getElementById("new-task-priority");
   const list = document.getElementById('list');
   
   createTaskForm.addEventListener('submit', (e) => {
